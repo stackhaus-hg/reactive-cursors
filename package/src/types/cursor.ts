37a13a2ea67b783eaacce9f7fa073188d17bfa16
marcobuontempo <marco.buon36@gmail.com>
2025-09-11 02:40:21 +1000

    Create prototype cursor

--- package/src/types/cursor.ts
+++ package/src/types/cursor.ts
@@ -1,7 +1,9 @@
 import { FC, SVGProps } from "react";
 
+export type SvgDefaultStyles = "arrow" | "circle" | "cross";
+
 export type CursorLayer = {
-  SVG?: FC<SVGProps<SVGSVGElement>> | string;
+  SVG?: FC<SVGProps<SVGSVGElement>> | SvgDefaultStyles;
   fill?: string;
   stroke?: string;
   strokeSize?: number;
